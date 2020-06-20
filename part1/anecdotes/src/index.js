a2159c7061ef7660e5938fae5c08123d7af7e6fc
Oto Zakalashvili <oto.zakalashvili@gmail.com>
2020-06-20 21:43:20 +0400

    Adding vote and rating features to anecdotes app.

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -3,16 +3,31 @@ import ReactDOM from "react-dom";
 
 const App = props => {
   const [selected, setSelected] = useState(0);
+  const [points, setPoints] = useState(new Array(6).fill(0));
 
   const nextAnecdote = () => {
     setSelected(Math.floor(Math.random() * 6));
   };
 
+  const vote = selected => {
+    setPoints([
+      ...points.slice(0, selected),
+      points[selected] + 1,
+      ...points.slice(selected + 1, points.length)
+    ]);
+  };
+
   return (
     <div>
+      <h1>Anecdote of the day </h1>
       {props.anecdotes[selected]}
       <br />
+      <p> has {points[selected]} points </p>
+      <button onClick={() => vote(selected)}>vote</button>
       <button onClick={nextAnecdote}>next anecdote</button>
+
+      <h1>Anecdote with most votes</h1>
+      {props.anecdotes[points.indexOf(Math.max.apply(null, points))]}
     </div>
   );
 };
